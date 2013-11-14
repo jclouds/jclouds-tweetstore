638a047361f453c9767a4cc24946ba5dffbdfc05
Andrew Phillips <demobox1@yahoo.co.uk>
2013-11-14 01:01:25 +0100

    Updating cloudfoundry-spring to Cloud Foundry v2

--- cloudfoundry-spring/src/test/java/org/jclouds/demo/tweetstore/integration/CloudFoundryServer.java
+++ cloudfoundry-spring/src/test/java/org/jclouds/demo/tweetstore/integration/CloudFoundryServer.java
@@ -23,6 +23,7 @@ import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.io.Closeables.close;
 import static java.lang.String.format;
 import static org.jclouds.demo.tweetstore.integration.util.Zips.zipDir;
+import static org.jclouds.http.Uris.uriBuilder;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -30,9 +31,12 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
-import org.cloudfoundry.client.lib.CloudApplication.AppState;
+import org.cloudfoundry.client.lib.CloudCredentials;
 import org.cloudfoundry.client.lib.CloudFoundryClient;
+import org.cloudfoundry.client.lib.domain.CloudApplication;
+import org.cloudfoundry.client.lib.domain.CloudApplication.AppState;
 
 /**
  * Basic &quot;server facade&quot; functionality to deploy a WAR to Cloud Foundry.
@@ -40,31 +44,32 @@ import org.cloudfoundry.client.lib.CloudFoundryClient;
  * @author Andrew Phillips
  */
 public class CloudFoundryServer {
-    private static final String CLOUD_FOUNDRY_APPLICATION_URL_SUFFIX = ".cloudfoundry.com";
+    private static final String CLOUD_FOUNDRY_APPLICATION_URL_SUFFIX = ".cfapps.io";
+    private static final int MAX_STATUS_CHECKS = 20;
     
     protected CloudFoundryClient client;
     protected String appName;
     
     public void writePropertiesAndStartServer(final String address, final String warfile, 
-            String target, String username, String password, Properties props) throws IOException, InterruptedException, ExecutionException {
+            String target, String username, String password, Properties props) throws IOException, InterruptedException, ExecutionException, TimeoutException {
         String propsfile = String.format("%1$s/WEB-INF/jclouds.properties", warfile);
         System.err.println("file: " + propsfile);
         storeProperties(propsfile, props);
         assert new File(propsfile).exists();
 
-        client = new CloudFoundryClient(username, password, target);
+        CloudCredentials credentials = new CloudCredentials(username, password);
+        client = new CloudFoundryClient(credentials,  uriBuilder(target).build().toURL());
         client.login();
         appName = getAppName(address);
         deploy(warfile);
+        waitForInstance(appName);
         client.logout();
-        TimeUnit.SECONDS.sleep(10);
     }
 
     private void deploy(String explodedWar) throws IOException {
         File war = zipDir(explodedWar, format("%s-cloudfoundry.war", explodedWar));
         client.uploadApplication(appName, war); 
-        
-        // adapted from https://github.com/cloudfoundry/vcap-java-client/blob/master/cloudfoundry-maven-plugin/src/main/java/org/cloudfoundry/maven/Update.java
+        // adapted from https://github.com/cloudfoundry/cf-java-client/blob/master/cloudfoundry-maven-plugin/src/main/java/org/cloudfoundry/maven/AbstractPush.java
         AppState appState = client.getApplication(appName).getState();
         switch (appState) {
         case STOPPED:
@@ -76,6 +81,21 @@ public class CloudFoundryServer {
         default:
             throw new IllegalStateException(format("Unexpected application state '%s'", appState));
         }
+    }
+
+    private void waitForInstance(String appName) throws TimeoutException, InterruptedException {
+        int statusChecks = 0;
+        CloudApplication application;
+        while (statusChecks < MAX_STATUS_CHECKS) {
+           application = client.getApplication(appName);
+           if (application.getRunningInstances() == application.getInstances()) {
+              return;
+           }
+           statusChecks++;
+           TimeUnit.SECONDS.sleep(1);
+        }
+        // should have returned before we get here if the app started
+        throw new TimeoutException(format("Application '%s' not started after %d checks", appName, MAX_STATUS_CHECKS));   
     }
 
     private static void storeProperties(String filename, Properties props)
